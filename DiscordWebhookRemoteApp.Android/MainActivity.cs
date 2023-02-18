d2f485fbff5c48a5904e5d8ff414af2c9c944902
BySuspect <omerbayraktar43@gmail.com>
2023-02-18 18:45:42 +0300

    android nugetlerini 1 version düşürdüm hata çözüldü splash screen çalışıyor

--- DiscordWebhookRemoteApp.Android/MainActivity.cs
+++ DiscordWebhookRemoteApp.Android/MainActivity.cs
@@ -8,18 +8,18 @@ using Android.Gms.Ads;
 
 namespace DiscordWebhookRemoteApp.Droid
 {
-    [Activity(Label = "Discord Webhook Remote", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "Discord Webhook Remote", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     [IntentFilter(new[] { Xamarin.Essentials.Platform.Intent.ActionAppAction }, Categories = new[] { Android.Content.Intent.CategoryDefault })]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            MobileAds.Initialize(ApplicationContext);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
+            MobileAds.Initialize(ApplicationContext);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ DiscordWebhookRemoteApp.Android/SplashActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace DiscordWebhookRemoteApp.Droid
+{
+    [Activity(Label = "Discord Webhook Remote", Theme = "@style/SplashTheme", Icon = "@mipmap/icon", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            this.StartActivity(typeof(MainActivity));
+        }
+    }
+}
