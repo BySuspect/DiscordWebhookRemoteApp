818a4c08259ef561355f6f3026098c73e46f4765
Omer Y. Bayraktar <70052500+BySuspect@users.noreply.github.com>
2024-04-03 12:03:43 +0300

    beta1.4 release

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,44 +1,9 @@
 ﻿using CommunityToolkit.Maui;
-/* Unmerged change from project 'DiscordWebhookRemoteApp (net8.0-maccatalyst)'
-Before:
 using DiscordWebhookRemoteApp.Handlers;
+using Plugin.MauiMTAdmob;
+#if DEBUG
 using Microsoft.Extensions.Logging;
-using Plugin.MauiMTAdmob;
-After:
-using DiscordWebhookRemoteApp.Handlers;
-
-using Microsoft.Extensions.Logging;
-
-using Plugin.MauiMTAdmob;
-*/
-
-/* Unmerged change from project 'DiscordWebhookRemoteApp (net8.0-ios)'
-Before:
-using DiscordWebhookRemoteApp.Handlers;
-using Microsoft.Extensions.Logging;
-using Plugin.MauiMTAdmob;
-After:
-using DiscordWebhookRemoteApp.Handlers;
-
-using Microsoft.Extensions.Logging;
-
-using Plugin.MauiMTAdmob;
-*/
-
-/* Unmerged change from project 'DiscordWebhookRemoteApp (net8.0-windows10.0.19041.0)'
-Before:
-using DiscordWebhookRemoteApp.Handlers;
-using Microsoft.Extensions.Logging;
-using Plugin.MauiMTAdmob;
-After:
-using DiscordWebhookRemoteApp.Handlers;
-
-using Microsoft.Extensions.Logging;
-
-using Plugin.MauiMTAdmob;
-*/
-using DiscordWebhookRemoteApp.Handlers;
-using Plugin.MauiMTAdmob;
+#endif
 
 namespace DiscordWebhookRemoteApp
 {
